8c899d125b2dfeb39b7ed463e09208d7140e1347
limon-l <limonroyapu.edu@gmail.com>
2026-07-15 14:56:06 +0600

    backend issue fix

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   async rewrites() {
     if (process.env.NEXT_PUBLIC_API_URL) return [];
     return [
